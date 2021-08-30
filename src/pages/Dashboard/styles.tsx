eafc80a5d288bf5bebfdc93db46e4c009f22d517
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-08-30 15:00:50 -0300

    lesson 7: Styled Components #1 & #2

---
+++ src/pages/Dashboard/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  color: #3a3a3a;
+  max-width: 30rem;
+  line-height: 3.5rem;
+  margin-top: 4rem;
+`;
