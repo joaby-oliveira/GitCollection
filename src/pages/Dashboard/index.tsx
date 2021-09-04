b1782ed8d8a6373277b2209fa4beaca39b375179
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-04 19:12:11 -0300

    lesson: create repo details page #2

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import { Title, Form, Repos, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi'
 import logo from '../../assets/logo.svg';
 import { api } from '../../services/api';
-
+import {Link} from 'react-router-dom'
 
 interface GithubRepository {
   full_name: string;
@@ -65,14 +65,14 @@ export const Dashboard: FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repos>
         {repos.map(repository => (
-          <a href={`repositories/${repository.full_name}`} key={repository.full_name}>
+          <Link to={`repositories/${repository.full_name}`} key={repository.full_name}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
             <div>
               <strong>{repository.full_name}</strong>
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repos>
     </>
--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { useRouteMatch } from 'react-router-dom'
+import { Link, useRouteMatch } from 'react-router-dom'
+
+import { Header } from './styles'
+import logo from '../../assets/logo.svg'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 interface RepositoryParams {
   repository: string;
 }
 
+
+
 export const Repo: React.FC = () => {
-  const {params} = useRouteMatch<RepositoryParams>();
+  const {params} = useRouteMatch<RepositoryParams>()
+  
   return (
-    <h1>Repo: {params.repository}</h1>
+    <>
+      <Header>
+        <img src={logo}/>
+        <Link to="/">
+          <FiChevronLeft/>
+          Voltar
+        </Link>
+      </Header>
+    </>
   )
 }
